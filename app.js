cec44c372f23c05d60274d90702941db6954a896
indiciumx <chris@indiciumx.com>
2024-02-14 18:10:31 -0400

    2402141810

--- app.js
+++ app.js
@@ -11,32 +11,39 @@ app.get('/', async (req, res) => {
 
   // Define the HTML content to be returned
   const htmlContent = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <title>Portail DPI Portal</title>
-    <script src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"></script>
-    <gen-search-widget
-      configId="e9a146aa-2908-4896-a424-d21837c24d46"
-      location="us"
-      triggerId="searchWidgetTrigger">
-    </gen-search-widget>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>`;
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Web site created using create-react-app"
+      />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+      <!--
+        manifest.json provides metadata used when your web app is installed on a
+        user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+      -->
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <title>Portail DPI Portal</title>
+      <!-- Widget JavaScript bundle -->
+  <script src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"></script>
+  
+  <!-- Search widget element is not visible by default -->
+  <gen-search-widget
+    configId="e9a146aa-2908-4896-a424-d21837c24d46"
+    location="us"
+    triggerId="searchWidgetTrigger">
+  </gen-search-widget>
+    </head>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root"></div>
+    </body>
+  </html>`;
 
   // Use res.send() to return the HTML content
   res.send(htmlContent);
